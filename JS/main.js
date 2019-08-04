3d430cac51ebf4ee86b55a3502b935c96ba0f316
Álvaro Perera <alvprodz@gmail.com>
2019-08-05 00:58:10 +0200

    Añadida funcionalidad Reactiva
    -He creado un campo de texto con un texto cambiante en tiempo real usando las propiedades de directivas de VUE

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+new Vue({
+	el:'#Mostrar',
+	data: {
+		mensaje: 'Prueba de Hola mundo en VUE',
+		nombreCambiante: 'Default'
+	}
+});
+
+new Vue({
+	el:'#Campo',
+	data: {
+		nombreCambiante: 'Default'
+	}
+});
